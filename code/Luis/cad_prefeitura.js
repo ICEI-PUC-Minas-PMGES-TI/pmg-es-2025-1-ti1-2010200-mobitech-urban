929666f119b4a20fef68b3cbc1f0a191f35bb5c0
Luis Gustavo de Souza Xavier <luisgustavoxavier1234@gmail.com>
2025-05-14 19:31:05 -0300

    Adição de mascaras ao cep e telefone

---
+++ code/Luis/cad_prefeitura.js
@@ -0,0 +1,27 @@
+const telefone = document.querySelector('#telefone');
+const cep = document.querySelector('#endereco');
+const telefoneMask = {
+      mask: '(00) 00000-0000'
+};
+const cepMask = {
+      mask: '00000-000'
+};
+
+
+IMask(cep,cepMask);
+IMask(telefone,telefoneMask);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
